4c3f054b603bf8275eee6e84d007108dd3d565fc
Jai-JAP <jai.jap.318@gmail.com>
2024-03-02 16:53:13 +0530

    Add @types/mongoose dependency and update module and module resolution in tsconfig.json

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,37 +2,30 @@ import mongoose, { Document, Model } from "mongoose";
 const { Schema, model, models } = mongoose;
 
 export interface UserDocument extends Document {
-  username: string,
-  password: string,
-  communities: Array<string>,
+  username: String,
+  password: String,
+  communities: Array<String>,
 }
-
-// const userCaseSchema = new Schema<userCaseAttrs>({
-//   caseID: {
-//     type: String,
-//   },
-//   role: {
-//     type: String,
-//   },
-// });
 
 const userSchema = new Schema<UserDocument>(
   {
     username: {
       type: String,
       unique: true,
-      required: true,
+      required: true
     },
     password: {
       type: String,
-      required: false,
+      required: false
     },
-    communities: [{
+    communities: [
+      {
         type: String,
-        required: false,
-    },]
+        required: false
+      }
+    ]
   },
-  { timestamps: true }  
+  { timestamps: true }
 );
 
 export const User: Model<UserDocument> = models.User
