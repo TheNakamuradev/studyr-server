63845ab1d6218ed629e343581209e3eb9c81ca6c
Jai-JAP <jai.jap.318@gmail.com>
2024-03-03 01:48:46 +0530

    Add SessionDocument interface and Sessions array to CommunityDocument. Add communityinfo endpoint

--- src/models/Community.ts
+++ src/models/Community.ts
@@ -2,12 +2,31 @@ import mongoose, { Document, Model } from "mongoose";
 
 const { Schema, model, models } = mongoose;
 
+interface SessionDocument extends Document {
+  name: string;
+}
+
 export interface CommunityDocument extends Document {
   name: string;
   description: string;
   admin: Array<string>;
   users: Array<string>;
+  Sessions: Array<SessionDocument>;
 }
+
+const sessionSchema = new Schema<SessionDocument>(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    _id: {
+      type: Schema.Types.ObjectId,
+      ref: "Session"
+    }
+  },
+  { timestamps: true }
+);
 
 const communitySchema = new Schema<CommunityDocument>(
   {
@@ -20,16 +39,22 @@ const communitySchema = new Schema<CommunityDocument>(
     },
     admin: [
       {
-        type: String,
+        type: Schema.Types.ObjectId,
         unique: true,
         required: true
       }
     ],
     users: [
       {
-        type: String,
+        type: Schema.Types.ObjectId,
         unique: true,
         required: true
+      }
+    ],
+    Sessions: [
+      {
+        type: sessionSchema,
+        unique: true
       }
     ]
   },
---
+++ src/routes/community/info.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+import { CommunityDocument, Community } from "../../models/Community";
+import { UserDocument, User } from "../../models/User";
+
+export default async function getCommunityInfo(req: Request, res: Response) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    res.status(401).send({ message: "Unauthorized Access" });
+    return;
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET || "secret") as {
+      userId: string;
+    };
+    if (!(await User.findById(decodedToken.userId))) {
+      res.status(403).send({ message: "Unauthorised Access" });
+      return;
+    }
+
+    const communityId = req.params.communityId;
+    const community = await Community.findById(communityId);
+    if (!community) {
+      res.status(404).send({ status: "error", message: "Community Not Found" });
+      return;
+    }
+
+    res.status(200).send({
+      communityId,
+      name: community.name,
+      description: community.description,
+      admin: community.admin,
+      users: community.users
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(404).send({ status: "error", message: "Community Not Found" });
+  }
+}
