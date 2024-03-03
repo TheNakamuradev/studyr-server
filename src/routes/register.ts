4990631aff668884eb16981a64b661f2bb5b5f1f
Jai-JAP <jai.jap.318@gmail.com>
2024-03-03 17:07:13 +0530

    Refactor register route to handle login parameter

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -4,11 +4,11 @@ import jwt from "jsonwebtoken";
 import { UserDocument, User } from "../models/User";
 
 export default async function register(req: Request, res: Response) {
-  const { name, username, password, login } = req.body as {name:string , username: string, password: string, login: boolean | undefined};
+  const { name, username, password, login } = req.body as { name: string, username: string, password: string, login: boolean | undefined };
   try {
     const user: UserDocument = new User({ name, username, password });
     await user.save();
-    if (login === undefined || login === true) {
+    if (login !== false) {
       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET || "secret");
       res.status(201).send({ user: { username, password: user.password }, token: token });
     } else {
